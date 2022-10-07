e1d09fbcf2af53edec43b29e499c28dbd7e1617b
vadic1234 <vadicesteba@inspladelestany.cat>
2022-10-07 15:57:43 +0100

    fitxer usuaris

---
+++ Usuari.java
@@ -0,0 +1,11 @@
+public class Usuari {
+    private String nomDelUsuari;
+
+    public Usuari(String nomUsuari) {
+        this.nomDelUsuari = nomUsuari;
+    }
+
+    public String getNomDelUsuari() {
+        return nomDelUsuari;
+    }
+}
